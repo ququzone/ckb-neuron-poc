1272c0b4dd49c4cc0e7bd49cfaa38a8b8506f5f9
ququzone <xueping.yang@gmail.com>
2020-01-05 22:21:22 +0800

    complete http secp256k1 single info

--- packages/express/src/plugin/secp256k1-single-http.ts
+++ packages/express/src/plugin/secp256k1-single-http.ts
@@ -1,13 +1,35 @@
 import { Action } from "ckb-neuron-poc-service/lib/plugins";
 import { Secp256k1SinglePlugin } from "ckb-neuron-poc-service/lib/plugins/secp256k1";
+import axios from "axios";
+import BigNumber from "bignumber.js";
 
 export class HttpSecp256k1Plugin extends Secp256k1SinglePlugin {
-  public constructor(privateKey: string, actions: Action[]) {
+  private url: string;
+  private privateKey: string;
+
+  public constructor(url: string, privateKey: string, actions: Action[]) {
     super(privateKey, actions);
+    this.privateKey = privateKey;
+    this.url = url;
   }
 
   public async info(): Promise<string> {
-    
-    return "";
+    // TODO only first 100 cells
+    const response = await axios.get(`${this.url}/cells?lockHash=${this.lock.hash()}`);
+
+    const total = response.data.reduce((sum: BigNumber, cell: any) => {
+      return sum.plus(new BigNumber(cell.capacity, 16));
+    }, new BigNumber(0));
+    return `${this.lock.hash()} balance is: ${total}`;
   }
 }
+
+export default function run() {
+  const plugin = new HttpSecp256k1Plugin("http://localhost:3000", "0x86c5661a58a0589009a600b9008ec083ddf65f0b8e194aa2b1d5178fbdf8122f", []);
+
+  plugin.info().then(info => {
+    console.log(info);
+  });
+}
+
+run();
