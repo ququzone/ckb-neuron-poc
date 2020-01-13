85da220aee160d881db2b1798ec712ca78e6b092
ququzone <xueping.yang@gmail.com>
2020-01-13 21:22:31 +0800

    fix data little endian

--- packages/express/src/plugin/udt/balance.ts
+++ packages/express/src/plugin/udt/balance.ts
@@ -22,6 +22,15 @@ export default async function run() {
   );
   info = await plugin.info();
   console.log(info);
+
+  plugin = new SimpleUDTPlugin(
+    "http://localhost:3000",
+    "0x6a242b57227484e904b4e08ba96f19a623c367dcbd18675ec6f2a71a0ff4ec26",
+    "0x5a59abed0d6fcbfb58b48dcabb4ebae9b91432a3fe1311964bc96b2e410d7da7",
+    []
+  );
+  info = await plugin.info();
+  console.log(info);
 }
 
 run();
--- packages/express/src/plugin/udt/simple-udt.ts
+++ packages/express/src/plugin/udt/simple-udt.ts
@@ -3,12 +3,7 @@ import { Secp256k1SinglePlugin, Secp256k1LockScript } from "ckb-neuron-poc-servi
 import CKB from "@nervosnetwork/ckb-sdk-core";
 import * as utils from "@nervosnetwork/ckb-sdk-utils";
 import axios from "axios";
-import BigNumber from "bignumber.js";
-
-export function leftPad(origin: string, chars: number): string {
-  const padding = (chars - origin.length + 1 >= 0) ? chars - origin.length + 1 : 0;
-  return `0x${new Array(padding).join("0") + origin}`;
-}
+import BN from "bn.js";
 
 export class UDTTypeScript implements TypeScript {
   public name: "UDTTypeScript";
@@ -68,7 +63,7 @@ export class IssueAction implements Action {
   async sign(totalSupply: string): Promise<CKBComponents.RawTransaction> {
     const deps = await this.ckb.loadSecp256k1Dep();
 
-    const total = new BigNumber("15000002000");
+    const total = new BN("15000002000");
 
     const rawTx = {
       version: "0x0",
@@ -89,16 +84,16 @@ export class IssueAction implements Action {
       inputs: [],
       outputs: [
         {
-          capacity: `0x${new BigNumber("15000000000").toString(16)}`,
+          capacity: `0x${new BN("15000000000").toString(16)}`,
           lock: this.lock,
           type: this.type,
         },
       ],
       witnesses: [],
-      outputsData: [leftPad(`${new BigNumber(totalSupply).toString(16)}`, 32)]
+      outputsData: [`0x${new BN(totalSupply).toBuffer("le", 16).toString("hex")}`]
     };
-    
-    let sum = new BigNumber(0);
+
+    let sum = new BN(0);
     // TODO only first 100 cells
     const response = await axios.get(`${this.cacheUrl}/cells?lockHash=${utils.scriptToHash(this.lock)}`);
     for (let i = 0; i < response.data.length; i++) {
@@ -107,7 +102,8 @@ export class IssueAction implements Action {
         continue;
       }
 
-      sum = sum.plus(new BigNumber(element.capacity, 16));
+      sum = sum.add(new BN(element.capacity.slice(2), 16));
+
       rawTx.inputs.push({
         previousOutput: {
           txHash: element.txHash,
@@ -119,9 +115,9 @@ export class IssueAction implements Action {
       if (sum.lt(total)) {
         continue;
       }
-      if (sum.gt(total) && sum.minus(total).gt(new BigNumber("6100000000"))) {
+      if (sum.gt(total) && sum.sub(total).gt(new BN("6100000000"))) {
         rawTx.outputs.push({
-          capacity: `0x${sum.minus(total).toString(16)}`,
+          capacity: `0x${sum.sub(total).toString(16)}`,
           lock: this.lock,
           type: undefined,
         });
@@ -179,7 +175,7 @@ export class BurnAction implements Action {
     throw new Error("Method not implemented.");
   }
 
-  async sign(totalSupply: string): Promise<CKBComponents.RawTransaction> {
+  async sign(): Promise<CKBComponents.RawTransaction> {
     const deps = await this.ckb.loadSecp256k1Dep();
 
     const rawTx = {
@@ -209,13 +205,13 @@ export class BurnAction implements Action {
       outputsData: ["0x"]
     };
     
-    let sum = new BigNumber(0);
+    let sum = new BN(0);
     // TODO only first 100 cells
     const response = await axios.get(`${this.cacheUrl}/cells?lockHash=${utils.scriptToHash(this.lock)}&typeHash=${utils.scriptToHash(this.type)}`);
     for (let i = 0; i < response.data.length; i++) {
       const element = response.data[i];
 
-      sum = sum.plus(new BigNumber(element.capacity, 16));
+      sum = sum.add(new BN(element.capacity.slice(2), 16));
       rawTx.inputs.push({
         previousOutput: {
           txHash: element.txHash,
@@ -225,7 +221,7 @@ export class BurnAction implements Action {
       });
       rawTx.witnesses.push("0x");
     }
-    rawTx.outputs[0].capacity = `0x${sum.minus(new BigNumber("1000")).toString(16)}`;
+    rawTx.outputs[0].capacity = `0x${sum.sub(new BN("1000")).toString(16)}`;
 
     rawTx.witnesses[0] = {
       lock: "",
@@ -271,9 +267,9 @@ export class TransferAction implements Action {
   async sign(to: CKBComponents.Script, amount: string, fee: string): Promise<CKBComponents.RawTransaction> {
     const deps = await this.ckb.loadSecp256k1Dep();
 
-    const bigAmount = new BigNumber(amount);
-    const totalCKB = new BigNumber("15000000000").plus(new BigNumber(fee));
-    const gatheredCKB = new BigNumber(0);
+    const bigAmount = new BN(amount);
+    const totalCKB = new BN("15000000000").add(new BN(fee));
+    const gatheredCKB = new BN(0);
 
     const rawTx = {
       version: "0x0",
@@ -294,22 +290,22 @@ export class TransferAction implements Action {
       inputs: [],
       outputs: [
         {
-          capacity: `0x${new BigNumber("15000000000").toString(16)}`,
+          capacity: `0x${new BN("15000000000").toString(16)}`,
           lock: to,
           type: this.type
         },
       ],
       witnesses: [],
-      outputsData: [leftPad(`${bigAmount.toString(16)}`, 32)]
+      outputsData: [`0x${bigAmount.toBuffer("le", 16).toString("hex")}`]
     };
   
-    let sum = new BigNumber(0);
+    let sum = new BN(0);
     // TODO only first 100 cells
     let response = await axios.get(`${this.cacheUrl}/cells?lockHash=${utils.scriptToHash(this.lock)}&typeHash=${utils.scriptToHash(this.type)}`);
     for (let i = 0; i < response.data.length; i++) {
       const element = response.data[i];
 
-      sum = sum.plus(new BigNumber(element.data.slice(2), 16));
+      sum = sum.add(new BN(Buffer.from(element.data.slice(2), "hex"), 16, "le"));
       rawTx.inputs.push({
         previousOutput: {
           txHash: element.txHash,
@@ -318,7 +314,7 @@ export class TransferAction implements Action {
         since: "0x0",
       });
       rawTx.witnesses.push("0x");
-      gatheredCKB.plus(new BigNumber(element.capacity, 16));
+      gatheredCKB.add(new BN(element.capacity.slice(2), 16));
 
       if (sum.lt(bigAmount)) {
         continue;
@@ -327,15 +323,15 @@ export class TransferAction implements Action {
 
     if (sum.gt(bigAmount)) {
       rawTx.outputs.push({
-        capacity: `0x${new BigNumber("15000000000").toString(16)}`,
+        capacity: `0x${new BN("15000000000").toString(16)}`,
         lock: this.lock,
         type: this.type
       });
-      rawTx.outputsData.push(leftPad(`${sum.minus(bigAmount).toString(16)}`, 32));
-      totalCKB.plus(new BigNumber("15000000000"));
+      rawTx.outputsData.push(`0x${sum.sub(bigAmount).toBuffer("le", 16).toString("hex")}`);
+      totalCKB.add(new BN("15000000000"));
     }
 
-    sum = new BigNumber(gatheredCKB);
+    sum = new BN(gatheredCKB);
     // TODO only first 100 cells
     response = await axios.get(`${this.cacheUrl}/cells?lockHash=${utils.scriptToHash(this.lock)}`);
     for (let i = 0; i < response.data.length; i++) {
@@ -345,7 +341,7 @@ export class TransferAction implements Action {
         continue;
       }
 
-      sum = sum.plus(new BigNumber(element.capacity, 16));
+      sum = sum.add(new BN(element.capacity.slice(2), 16));
       rawTx.inputs.push({
         previousOutput: {
           txHash: element.txHash,
@@ -354,14 +350,14 @@ export class TransferAction implements Action {
         since: "0x0",
       });
       rawTx.witnesses.push("0x");
-      gatheredCKB.plus(new BigNumber(element.capacity, 16));
+      gatheredCKB.add(new BN(element.capacity.slice(2), 16));
 
       if (sum.lt(totalCKB)) {
         continue;
       }
-      if (sum.gt(totalCKB) && sum.minus(totalCKB).gt(new BigNumber("6100000000"))) {
+      if (sum.gt(totalCKB) && sum.sub(totalCKB).gt(new BN("6100000000"))) {
         rawTx.outputs.push({
-          capacity: `0x${sum.minus(totalCKB).toString(16)}`,
+          capacity: `0x${sum.sub(totalCKB).toString(16)}`,
           lock: this.lock,
           type: undefined
         });
@@ -424,9 +420,9 @@ export class SimpleUDTPlugin extends Secp256k1SinglePlugin {
     // TODO only first 100 cells
     const response = await axios.get(`${this.url}/cells?lockHash=${this.lock.hash()}&typeHash=${this.type.hash()}`);
 
-    const total = response.data.reduce((sum: BigNumber, cell: any) => {
-      return sum.plus(new BigNumber(cell.data.slice(2), 16));
-    }, new BigNumber(0));
+    const total = response.data.reduce((sum: BN, cell: any) => {
+      return sum.add(new BN(Buffer.from(cell.data.slice(2), "hex"), 16, "le"));
+    }, new BN(0));
     return `${this.lock.hash()} balance is: ${total}`;
   }
 }
