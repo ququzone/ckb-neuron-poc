efbee143061da8963ae7f41159e2a5a4f06b7159
ququzone <xueping.yang@gmail.com>
2020-01-20 13:01:28 +0800

    add query and query builder

--- packages/service/src/cache/index.ts
+++ packages/service/src/cache/index.ts
@@ -1,8 +1,94 @@
+import * as BN from "bn.js";
 import CKB from "@nervosnetwork/ckb-sdk-core";
 import { Rule } from "../database/entity/rule";
 import { Cell } from "../database/entity/cell";
 import SyncService from "./sync";
 import CellRepository from "../database/cell-repository";
+
+export class Query {
+  lockHash: string;
+  lockCodeHash: string;
+  typeHash: string;
+  typeCodeHash: string;
+  capacity: BN;
+  capacityFetcher: (cell: Cell) => BN;
+
+  constructor(queryBuilder: QueryBuilder) {
+    this.lockHash = queryBuilder.lockHash;
+    this.lockCodeHash = queryBuilder.lockCodeHash;
+    this.typeHash = queryBuilder.typeHash;
+    this.typeCodeHash = queryBuilder.typeCodeHash;
+    this.capacity = queryBuilder.capacity;
+    this.capacityFetcher = queryBuilder.capacityFetcher;
+  }
+}
+
+export class QueryBuilder {
+  private _lockHash: string;
+  private _lockCodeHash: string;
+  private _typeHash: string;
+  private _typeCodeHash: string;
+  private _capacity: BN;
+  private _capacityFetcher: (cell: Cell) => BN;
+
+  build(): Query {
+    return new Query(this);
+  }
+
+  setLockHash(lockHash: string): QueryBuilder {
+    this._lockHash = lockHash;
+    return this;
+  }
+
+  setLockCodeHash(lockCodeHash: string): QueryBuilder {
+    this._lockCodeHash = lockCodeHash;
+    return this;
+  }
+
+  setTypeHash(typeHash: string): QueryBuilder {
+    this._typeHash = typeHash;
+    return this;
+  }
+
+  setTypeCodeHash(typeCodeHash: string): QueryBuilder {
+    this._typeCodeHash = typeCodeHash;
+    return this;
+  }
+
+  setCapacity(capacity: BN): QueryBuilder {
+    this._capacity = capacity;
+    return this;
+  }
+
+  setCapacityFetcher(capacityFetcher: (cell: Cell) => BN): QueryBuilder {
+    this._capacityFetcher = capacityFetcher;
+    return this;
+  }
+
+  get lockHash() {
+    return this._lockHash;
+  }
+
+  get lockCodeHash() {
+    return this._lockCodeHash;
+  }
+
+  get typeHash() {
+    return this._typeHash;
+  }
+
+  get typeCodeHash() {
+    return this._typeCodeHash;
+  }
+
+  get capacity() {
+    return this._capacity;
+  }
+
+  get capacityFetcher() {
+    return this._capacityFetcher;
+  }
+}
 
 export interface CacheService {
   addRule(rule: Rule): Promise<void>;
